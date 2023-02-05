3659c3334f83f2a3c8297b23054c396fde1b2b96
GeKa773 <GeKaRadchenko@gmail.com>
2023-02-05 19:44:48 +0300

    добавлены запросы для блогов

--- src/main/kotlin/data/blog/BlogData.kt
+++ src/main/kotlin/data/blog/BlogData.kt
@@ -1,4 +1,18 @@
 package data.blog
 
-data class BlogRequestRemote(val test: String)
-data class BlogResponseRemote(val id: Int, val test: String)
+import data.user.UserData
+
+data class BlogRequestRemote(
+    val title: String,
+    val body: String?,
+    val image: String?,
+)
+
+data class BlogResponseRemote(
+    val id: Int,
+    val user: UserData,
+    val title: String,
+    val body: String?,
+    val image: String?,
+    val likes: Int,
+)
--- src/main/kotlin/data/user/UserData.kt
+++ src/main/kotlin/data/user/UserData.kt
@@ -4,9 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserData(
-    val id: Int,
     val login: String,
+    val username: String,
     val email: String,
     val icon: String?,
-    val token: String,
 )
