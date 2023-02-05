789b8df42764149635b1cf657dbb83ed692ea649
GeKaR <GeKaRadchenko@gmail.com>
2023-02-06 00:35:58 +0300

    добавлено удаление блога

--- src/main/kotlin/data/blog/BlogData.kt
+++ src/main/kotlin/data/blog/BlogData.kt
@@ -1,13 +1,16 @@
 package data.blog
 
 import data.user.UserData
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class BlogRequestRemote(
     val title: String,
     val body: String?,
     val image: String?,
 )
 
+@Serializable
 data class BlogResponseRemote(
     val id: Int,
     val user: UserData,
